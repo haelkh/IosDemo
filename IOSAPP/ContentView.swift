f394bb44a17eb63f7f1c4637689b9a0ac57e8310
Hassan Elkhatib <elkhatibhassan42@gmail.com>
2025-05-23 05:12:59 +0300

    finished adding comments

--- IOSAPP/ContentView.swift
+++ IOSAPP/ContentView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import AVKit
-
+//powered by Hassan Elkhatib
 struct ContentView: View {
     @AppStorage("isSubscribed") private var isSubscribed: Bool = false
     @State private var showPlayer = false
